e4cbee874393804d0755def330426d523916272b
ducctrong <trongld21@gmail.com>
2022-06-14 12:35:04 +0700

    done landing page view for PC

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ $(document).ready(function() {
         nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 
     });
+
+    $('.masterstudy-blog-carousel').slick({
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+
+    });
 });
