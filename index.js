519a435a14b9d48fe3d0fca2b9d341aa88a24936
ducctrong <trongld21@gmail.com>
2022-07-07 03:11:33 +0700

    Add button back to top, add contact page

--- index.js
+++ index.js
@@ -16,4 +16,20 @@ $(document).ready(function() {
         nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
 
     });
+    var btn = $('#buttonBackToTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
 });
